72bbd052fc9e67afb9b77bfeab78c1a5787efece
Alicja <37802302+AlicjaWrobel@users.noreply.github.com>
2019-01-11 20:32:31 +0100

    Testy Alicji

---
+++ Program/hotel/src/test/java/Alicja/RoomTDAO.java
@@ -0,0 +1,13 @@
+package Alicja;
+
+import entities.Room;
+
+public class RoomTDAO {
+
+    public Room createRoom(Room room) {
+        if(room == null) {
+            return null;
+        }
+        if(R)
+    }
+}
